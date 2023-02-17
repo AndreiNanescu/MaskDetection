a9bc790c8e363951a89f52ff30b98dad96fdc09a
Nanescu <andrei.nanescu@porsche-engineering.ro>
2023-02-17 14:53:17 +0200

    -Overhauled the process_image function to use opencv2 -Added a validation set for better testing purposes of the NN

--- loadData.py
+++ loadData.py
@@ -1,35 +1,21 @@
-import os
 import pathlib
-import pickle
 import numpy as np
 import pandas as pd
+import sklearn.utils
 import tensorflow as tf
-from PIL import Image, ImageOps
-from matplotlib import rcParams
-import warnings
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-warnings.filterwarnings('ignore')
-rcParams['figure.figsize'] = (18, 8)
-rcParams['axes.spines.top'] = False
-rcParams['axes.spines.right'] = False
+import cv2
 
 
 def process_image(img_path: str) -> np.array:
-    img = Image.open(img_path)
-    img = ImageOps.grayscale(img)
-    img = img.resize(size=(96, 96))
+    img = cv2.imread(img_path)
+    img = cv2.resize(img, (96, 96))
     img = np.ravel(img) / 255.0
     return img
 
 
 def process_folder(folder: pathlib.PosixPath) -> pd.DataFrame:
-    # We'll store the images here
     processed = []
-
-    # For every image in the directory
     for img in folder.iterdir():
-        # Ensure JPG
         if img.suffix == '.jpg' or img.suffix == '.jpeg' or img.suffix == '.png':
             # Two images failed for whatever reason, so let's just ignore them
             try:
@@ -49,11 +35,21 @@ def load_data():
     train_face = process_folder(folder=pathlib.Path.cwd().joinpath('Data/Face'))
     train_mask = process_folder(folder=pathlib.Path.cwd().joinpath('Data/Mask'))
     train_set = pd.concat([train_face, train_mask], axis=0)
-    np.save('trainset.npy', train_set)
+    train_set = sklearn.utils.shuffle(train_set)
 
     x_train = train_set.drop('class', axis=1)
     y_train = train_set['class']
-    y_train.factorize()
-    y_train = tf.keras.utils.to_categorical(y_train.factorize()[0], num_classes=2)
-    x_train = x_train.values
-    return x_train, y_train
+    y_train_factorized = y_train.factorize()[0]
+    y_train = tf.keras.utils.to_categorical(y_train_factorized, num_classes=2)
+
+    valid_face = process_folder(folder=pathlib.Path.cwd().joinpath('Data/FaceValid'))
+    valid_mask = process_folder(folder=pathlib.Path.cwd().joinpath('Data/MaskValid'))
+    valid_set = pd.concat([valid_face, valid_mask], axis=0)
+    valid_set = sklearn.utils.shuffle(valid_set)
+
+    x_valid = valid_set.drop('class', axis=1)
+    y_valid = valid_set['class']
+    y_valid_factorized = y_valid.factorize()[0]
+    y_valid = tf.keras.utils.to_categorical(y_valid_factorized, num_classes=2)
+
+    return x_train, y_train, x_valid, y_valid
