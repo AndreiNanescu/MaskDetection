79942dd5f8ef374d4c5274f853d5f3c16cb940b7
Nanescu <andrei.nanescu@porsche-engineering.ro>
2023-03-14 17:25:48 +0200

    Hyperparameters tuning

--- loadData.py
+++ loadData.py
@@ -5,22 +5,24 @@ valid_dir = 'Data/validation'
 
 
 def load_data():
-    train = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1.0 / 255)
-    validation = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1.0 / 255)
+    train = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1.0 / 255,
+                                                            rotation_range=30,
+                                                            fill_mode='nearest',
+                                                            horizontal_flip=True,
+                                                            vertical_flip=True,
+                                                            )
+    validation = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1.0 / 255,
+                                                                 rotation_range=30,
+                                                                 fill_mode='nearest',
+                                                                 horizontal_flip=True,
+                                                                 vertical_flip=True,
+                                                                 )
     train_dataset = train.flow_from_directory(train_dir,
                                               target_size=(224, 224),
                                               batch_size=32,
-                                              rotation_range=30,
-                                              fill_mode='nearest',
-                                              horizontal_flip=True,
-                                              vertical_flip=True,
                                               class_mode='binary')
     valid_dataset = validation.flow_from_directory(valid_dir,
                                                    target_size=(224, 224),
                                                    batch_size=32,
-                                                   rotation_range=30,
-                                                   fill_mode='nearest',
-                                                   horizontal_flip=True,
-                                                   vertical_flip=True,
                                                    class_mode='binary')
     return train_dataset, valid_dataset
--- main.py
+++ main.py
@@ -1,29 +1,43 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from loadData import load_data
 
 train_set, valid_set = load_data()
 
-model = tf.keras.Sequential([tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(224, 224, 3)),
+model = tf.keras.Sequential([tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
                              tf.keras.layers.MaxPool2D(2, 2),
 
-                             tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+                             tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
                              tf.keras.layers.MaxPool2D(2, 2),
 
                              tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
                              tf.keras.layers.MaxPool2D(2, 2),
 
                              tf.keras.layers.Flatten(),
-                             tf.keras.layers.Dense(512, activation='relu'),
+                             tf.keras.layers.Dense(64, activation='relu'),
                              tf.keras.layers.Dense(1, activation='sigmoid')
                              ])
-
+model.summary()
 model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
               loss=tf.keras.losses.BinaryCrossentropy(),
               metrics=['accuracy'])
 
-file_name = 'my_saved_model'
-tensorboard = tf.keras.callbacks.TensorBoard(log_dir="logs\\{}".format(file_name))
-model = model.fit(train_set, validation_data=valid_set, epochs=10, callbacks=[tensorboard])
+history = model.fit(train_set, validation_data=valid_set, epochs=6)
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'val'], loc='upper left')
+plt.show()
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'val'], loc='upper left')
+plt.show()
 
 """
 vid = cv2.VideoCapture(0)
