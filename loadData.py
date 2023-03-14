03425319f84128fbbb7d4229b7e7c1234b12d1d4
Nanescu <andrei.nanescu@porsche-engineering.ro>
2023-03-14 14:06:23 +0200

    Added images to the train data set.

--- loadData.py
+++ loadData.py
@@ -3,16 +3,15 @@ import tensorflow as tf
 train_dir = 'Data/train'
 valid_dir = 'Data/validation'
 
-
 def load_data():
-    train = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1 / 255)
-    validation = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1 / 255)
+    train = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1.0 / 255)
+    validation = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1.0 / 255)
     train_dataset = train.flow_from_directory(train_dir,
                                               target_size=(224, 224),
-                                              batch_size=20,
+                                              batch_size=32,
                                               class_mode='binary')
     valid_dataset = validation.flow_from_directory(valid_dir,
                                                    target_size=(224, 224),
-                                                   batch_size=20,
+                                                   batch_size=32,
                                                    class_mode='binary')
     return train_dataset, valid_dataset
