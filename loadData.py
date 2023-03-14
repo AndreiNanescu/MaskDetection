4e2f31b52173068551196fd757da9c45167b9087
Nanescu <andrei.nanescu@porsche-engineering.ro>
2023-03-14 13:14:24 +0200

    Made a load_data function that can be called in the main

--- loadData.py
+++ loadData.py
@@ -1,26 +1,18 @@
-import pathlib
-import numpy as np
-import pandas as pd
-import sklearn.utils
 import tensorflow as tf
-import cv2
 
 train_dir = 'Data/train'
 valid_dir = 'Data/validation'
 
-train = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1 / 255)
-validation = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1 / 255)
 
-train_dataset = train.flow_from_directory(train_dir,
-                                          target_size=(224, 224),
-                                          batch_size=20,
-                                          class_mode='binary'
-                                          )
-valid_dataset = validation.flow_from_directory(valid_dir,
-                                               target_size=(224, 224),
-                                               batch_size=20,
-                                               class_mode='binary'
-                                               )
-train_dataset.class_indices
-valid_dataset.class_indices
-
+def load_data():
+    train = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1 / 255)
+    validation = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1 / 255)
+    train_dataset = train.flow_from_directory(train_dir,
+                                              target_size=(224, 224),
+                                              batch_size=20,
+                                              class_mode='binary')
+    valid_dataset = validation.flow_from_directory(valid_dir,
+                                                   target_size=(224, 224),
+                                                   batch_size=20,
+                                                   class_mode='binary')
+    return train_dataset, valid_dataset
