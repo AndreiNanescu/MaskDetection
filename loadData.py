7760f3f964c2c81d71a3d9b28ec69df4cf5d9fb5
Nanescu <andrei.nanescu@porsche-engineering.ro>
2023-03-20 11:27:51 +0200

    Hyperparameters tuning

--- loadData.py
+++ loadData.py
@@ -1,28 +1,21 @@
 import tensorflow as tf
 
-train_dir = 'Data/train'
-valid_dir = 'Data/validation'
+data_dir = 'Data'
 
 
 def load_data():
-    train = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1.0 / 255,
-                                                            rotation_range=30,
-                                                            fill_mode='nearest',
-                                                            horizontal_flip=True,
-                                                            vertical_flip=True,
-                                                            )
-    validation = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1.0 / 255,
-                                                                 rotation_range=30,
-                                                                 fill_mode='nearest',
-                                                                 horizontal_flip=True,
-                                                                 vertical_flip=True,
-                                                                 )
-    train_dataset = train.flow_from_directory(train_dir,
-                                              target_size=(224, 224),
-                                              batch_size=32,
-                                              class_mode='binary')
-    valid_dataset = validation.flow_from_directory(valid_dir,
-                                                   target_size=(224, 224),
-                                                   batch_size=32,
-                                                   class_mode='binary')
+    datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1 / 255,
+                                                              validation_split=0.2)
+
+    train_dataset = datagen.flow_from_directory(data_dir,
+                                                target_size=(224, 224),
+                                                batch_size=32,
+                                                class_mode='binary',
+                                                subset='training')
+    valid_dataset = datagen.flow_from_directory(data_dir,
+                                                target_size=(224, 224),
+                                                batch_size=32,
+                                                class_mode='binary',
+                                                subset='validation')
+
     return train_dataset, valid_dataset
--- main.py
+++ main.py
@@ -4,40 +4,42 @@ from loadData import load_data
 
 train_set, valid_set = load_data()
 
-model = tf.keras.Sequential([tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
+model = tf.keras.Sequential([tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3),
+                                                    kernel_regularizer=tf.keras.regularizers.l2(0.01)),
                              tf.keras.layers.MaxPool2D(2, 2),
 
-                             tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+                             tf.keras.layers.Conv2D(64, (3, 3), activation='relu',
+                                                    kernel_regularizer=tf.keras.regularizers.l2(0.01)),
                              tf.keras.layers.MaxPool2D(2, 2),
 
-                             tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-                             tf.keras.layers.MaxPool2D(2, 2),
+                             # tf.keras.layers.Conv2D(64, (3, 3), activation='relu',
+                             # kernel_regularizer=tf.keras.regularizers.l2(0.01)),
+                             # tf.keras.layers.MaxPool2D(2, 2),
 
                              tf.keras.layers.Flatten(),
-                             tf.keras.layers.Dense(64, activation='relu'),
+                             tf.keras.layers.Dense(64, activation='relu',
+                                                   kernel_regularizer=tf.keras.regularizers.l2(0.01)),
+                             tf.keras.layers.Dropout(0.50),
                              tf.keras.layers.Dense(1, activation='sigmoid')
                              ])
-model.summary()
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
               loss=tf.keras.losses.BinaryCrossentropy(),
               metrics=['accuracy'])
 
-history = model.fit(train_set, validation_data=valid_set, epochs=6)
+history = model.fit(train_set, steps_per_epoch=train_set.samples // 32, validation_data=valid_set,
+                    validation_steps=valid_set.samples // 32, epochs=20)
 
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'val'], loc='upper left')
+plt.plot(history.history['accuracy'], color='blue')
+plt.plot(history.history['val_accuracy'], color='blue', linestyle='--')
+plt.plot(history.history['loss'], color='orange')
+plt.plot(history.history['val_loss'], color='orange', linestyle='--')
+plt.title('Model accuracy and loss')
+plt.ylabel('Accuracy/Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train Acc', 'Val Acc', 'Train Loss', 'Val Loss'], loc='upper right')
 plt.show()
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'val'], loc='upper left')
-plt.show()
+
 
 """
 vid = cv2.VideoCapture(0)
