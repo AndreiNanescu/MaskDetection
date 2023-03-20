bf32d487eec5d24b55df71559f150ad6463936b2
Nanescu <andrei.nanescu@porsche-engineering.ro>
2023-03-20 16:13:50 +0200

    Created a separated file for the plotting code.

---
+++ plotting.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+
+
+def plot_data(history):
+    # plot model accuracy
+    plt.subplot(1, 2, 1)
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('Model Accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Validation'], loc='upper left')
+
+    # plot model loss
+    plt.subplot(1, 2, 2)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Validation'], loc='upper left')
+
+    # adjust layout and show plot
+    plt.subplots_adjust(wspace=0.3)
+    plt.show()
