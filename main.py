d9c73dcfbaf7e1416fab535714a888db2f6fe0b3
Nanescu <andrei.nanescu@porsche-engineering.ro>
2023-04-11 11:33:18 +0300

    Added an early stopping that monitors the validation loss

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from loadData import load_data
 from plotting import plot_data
+
 train_set, valid_set, test_set = load_data()
 
 model = tf.keras.Sequential([tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3),
@@ -31,8 +32,11 @@ model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
               loss=tf.keras.losses.BinaryCrossentropy(),
               metrics=['accuracy'])
 
+early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
 history = model.fit(train_set, steps_per_epoch=train_set.samples // 32, validation_data=valid_set,
-                    validation_steps=valid_set.samples // 32, epochs=30)
+                    validation_steps=valid_set.samples // 32, epochs=1, callbacks=[early_stop])
 
-print(model.predict(test_set))
+test_loss, test_acc = model.evaluate(test_set)
+print(f"Test accuracy {test_acc*100:.2f}%")
+
 plot_data(history)
